29688d53899792c56eba345106e99aa7a6dfca99
RicardoMFC <rich10fernandezconde@gmail.com>
2022-12-09 12:32:26 +0100

    Create Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,25 @@
+def stuart(texto):
+    texto_auxiliar=[]
+    for i in range(1,len(texto)):
+        if texto[i]!='a' and texto[i]!='e' and texto[i]!='i' and texto[i]!='o' and texto[i]!='u':
+            texto_auxiliar.append(texto[i])
+            for j in range(1,)
+
+
+
+
+
+
+
+
+
+
+def main():
+    cadena1=stuart['b','a','n','a','n','a']
+    cadena2=kevin['b','a','n','a','n','a']
+
+
+
+
+if __name__=='__main__':
+    main()
