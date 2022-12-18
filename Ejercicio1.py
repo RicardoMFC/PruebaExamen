9eb9703b80fc7683892265552ce1565fb80fe1df
RicardoMFC <rich10fernandezconde@gmail.com>
2022-12-18 23:43:53 +0100

    e

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,26 +1,36 @@
-def stuart(texto):
-    texto_auxiliar=[]
-    for i in range(1,len(texto)):
-        if texto[i]!='a' and texto[i]!='e' and texto[i]!='i' and texto[i]!='o' and texto[i]!='u':
-            texto_auxiliar.append(texto[i])
-            for j in range(1,)
-
-
-
-
-
-
-
-
-
-
-def main():
-    cadena1=stuart['b','a','n','a','n','a']
-    cadena2=kevin['b','a','n','a','n','a']
-
-
-
-
-if __name__=='__main__':
-    main()
-
+# Definimos una función que tome una cadena de caracteres y devuelva un diccionario con las subcadenas y su puntuación
+def minion_game(string):
+  # Creamos un diccionario vacío que almacenará las subcadenas y su puntuación
+  substrings = {}
+  
+  # Iteramos sobre todas las letras de la cadena
+  for i in range(len(string)):
+    # Stuart forma palabras que comiencen con consonantes
+    if string[i] not in "AEIOU":
+      # Iteramos sobre todas las subcadenas que comiencen con la letra actual
+      for j in range(i + 1, len(string) + 1):
+        # Agregamos la subcadena al diccionario y aumentamos su puntuación en 1
+        substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+        
+    # Kevin forma palabras que comiencen con vocales
+    else:
+      # Iteramos sobre todas las subcadenas que comiencen con la letra actual
+      for j in range(i + 1, len(string) + 1):
+        # Agregamos la subcadena al diccionario y aumentamos su puntuación en 1
+        substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+  return substrings
+# Probamos la función con la cadena "BANANA"
+solucion=minion_game("BANANA")
+print(solucion)
+# Deberíamos obtener el siguiente diccionario como resultado:
+# {
+#   "B": 1,
+#   "BA": 1,
+#   "BAN": 1,
+#   "BANA": 1,
+#   "AN": 2,
+#   "ANA": 2,
+#   "N": 2,
+#   "NA": 2,
+#   "A": 3
+# }
